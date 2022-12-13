13dbf3727be0cf9d54bf188e39cef83b2c067e90
actioninja <actioninja@criticalaction.net>
2022-12-12 16:02:38 -0800

    day 3 part 1

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,5 +1,47 @@
+use itertools::Itertools;
+use lazy_static::lazy_static;
+use std::collections::{HashMap, HashSet};
+
+lazy_static! {
+    static ref PRIORITIES: HashMap<char, u32> = {
+        let mut m = HashMap::new();
+        let mut counter = 0;
+        for c in 'a'..='z' {
+            counter += 1;
+            m.insert(c, counter);
+        }
+        for c in 'A'..='Z' {
+            counter += 1;
+            m.insert(c, counter);
+        }
+        m
+    };
+}
+
+fn get_packs(input: &str) -> Vec<(&str, &str)> {
+    input
+        .lines()
+        .map(|line| {
+            let split_point = line.len() / 2;
+            line.split_at(split_point)
+        })
+        .collect()
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let packs = get_packs(input);
+
+    let sum = packs
+        .iter()
+        .map(|pack| {
+            let mut found = HashSet::new();
+            found.extend(pack.0.chars());
+            let dupe = pack.1.chars().find(|c| found.contains(c)).unwrap();
+            PRIORITIES.get(&dupe).unwrap()
+        })
+        .sum();
+
+    Some(sum)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
